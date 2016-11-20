ded447ebdd41bf6e751dd04e2d734cc54fc531f5
Jean de Klerk <jadekler@gmail.com>
2016-11-20 15:31:54 -0700

    Deploy to CF

---
+++ bin/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cf api api.run.pivotal.io
+cf login
+cf push
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname});
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('App listening on port 3000!')
 })

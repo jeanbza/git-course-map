e2eec66ebac19b6345de34f18a6a0c248345967c
Jean de Klerk <jadekler@gmail.com>
2016-11-20 15:25:19 -0700

    Initial

--- src/components/CoursesApp.js
+++ src/components/CoursesApp.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
+import Course from './Course'
+import courses from '../courses'
+
+import Specialization from './Specialization'
+import specializations from '../specializations'
+
+const coursesList = courses.map(course => <Course key={course.code} code={course.code} name={course.name}/>)
+const specializationList = specializations.map(specialization => <Specialization key={specialization.name} name={specialization.name}/>)
+
 export default () => (
-  <div>Hello world</div>
+  <div id="app">
+    <div id="courses">
+      <h2>Courses</h2>
+      {coursesList}
+    </div>
+    <div id="specializations">
+      <h2>Specializations</h2>
+      {specializationList}
+    </div>
+  </div>
 )
---
+++ src/specializations.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    name: 'Computational Perception and Robotics',
+    required: [
+      {
+        type: 'one-of',
+        courses: ['CS 6505', 'CS 6520', 'CS 6550', 'CS 7520', 'CS 7530', 'CSE 6140']
+      },
+      {
+        type: 'one-of',
+        courses: ['CS 6601', 'CS 7641']
+      },
+      {
+        type: 'three-total-one-of-both',
+        subcategories: [
+          {
+            courses: ['CS 6475', 'CS 6476', 'CS 7499', 'CS 7636', 'CS 7650', 'CS 8803']
+          },
+          {
+            courses: ['CS 7630', 'CS 7631', 'CS 7633', 'CS 7649', 'CS 8803-O01']
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Computing Systems',
+    required: [
+      {
+        type: 'all',
+        courses: ['CS 6505']
+      },
+      {
+        type: 'two-of',
+        courses: ['CS 6210', 'CS 6241', 'CS 6250', 'CS 6290', 'CS 6300', 'CS 6400']
+      },
+      {
+        type: 'three-of',
+        courses: ['CS 6035', 'CS 6235', 'CS 6238', 'CS 6260', 'CS 6262', 'CS 6310', 'CS 6340', 'CS 6365', 'CS 6422', 'CS 6550', 'CS 6675', 'CS 7210', 'CS 7260', 'CS 7270', 'CS 7290', 'CS 7292', 'CS 7560', 'CS 8803-FPL']
+      }
+    ]
+  },
+  {
+    name: 'Interactive Intelligence',
+    required: [
+      {
+        type: 'one-of',
+        courses: ['CS 6300', 'CS 6505']
+      },
+      {
+        type: 'two-of',
+        courses: ['CS 6601', 'CS 7620', 'CS 7637', 'CS 7641']
+      },
+      {
+        type: 'two-of',
+        courses: ['CS 6440', 'CS 6460', 'CS 6465', 'CS 7634', 'CS 8803', 'CS 7632', 'CS 7650', 'CS 8803', 'CS 6795', 'CS 7610', 'CS 8803']
+      }
+    ]
+  },
+  {
+    name: 'Machine Learning',
+    required: [
+      {
+        type: 'one-of',
+        courses: ['CS 6505', 'CS 6520', 'CS 6550', 'CS 7510', 'CS 7520', 'CS 7530', 'CSE 6140']
+      },
+      {
+        type: 'one-of',
+        courses: ['CS 7641', 'CSE 6740']
+      },
+      {
+        type: 'three-of',
+        courses: ['CS 7540', 'CS 7545', 'CS 7616', 'CS 7646', 'CS 7650', 'CS 8803', 'CSE 6240', 'CSE 6242']
+      }
+    ]
+  }
+]

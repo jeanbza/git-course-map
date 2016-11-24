6eef3ee9b5f56a55389d56258eff5b0154d3249a
Jean de Klerk <jadekler@gmail.com>
2016-11-24 12:33:33 -0700

    Moved header into react

--- src/components/CoursesApp.js
+++ src/components/CoursesApp.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
+import Header from '../components/Header'
 import CourseList from '../containers/CourseList'
 import SpecializationList from '../containers/SpecializationList'
 
 export default () => (
-  <div id="app">
-    <div id="courses">
-      <h2>Courses</h2>
-      <CourseList />
-    </div>
-    <div id="specializations">
-      <h2>Specializations</h2>
-      <SpecializationList />
+  <div>
+    <Header />
+    <div id="app">
+      <div id="courses">
+        <h2>Courses</h2>
+        <CourseList />
+      </div>
+      <div id="specializations">
+        <h2>Specializations</h2>
+        <SpecializationList />
+      </div>
     </div>
   </div>
 )
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <div className="header">
+    <a target="_blank" href="https://www.patreon.com/user?u=274157">Buy me a beer!</a>
+    <a target="_blank" href="https://github.com/jadekler/git-course-map/issues">Suggestions/feedback</a>
+    <a target="_blank" href="https://github.com/jadekler/git-course-map">Star/fork this on github!</a>
+  </div>
+)

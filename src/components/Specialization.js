cbc82fabae3f697678b8459f6df211dfb4b90085
Jean de Klerk <jadekler@gmail.com>
2016-11-27 14:43:03 -0700

    formatting

--- src/components/Specialization.js
+++ src/components/Specialization.js
@@ -4,8 +4,7 @@ import RequiredGroup from './RequiredGroup'
 
 export default ({specialization}) => {
   const {name, required} = specialization
-  const groups = required.map((group, i) => <RequiredGroup key={i}
-                                                           group={group}/>)
+  const groups = required.map((group, i) => <RequiredGroup key={i} group={group}/>)
 
   return (
     <div>

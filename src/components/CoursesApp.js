ef614529ec3988e963d121b0196ac94d162c50cd
Jean de Klerk <jadekler@gmail.com>
2016-11-27 15:42:53 -0700

    Foreword added and some misc styling

--- src/components/CoursesApp.js
+++ src/components/CoursesApp.js
@@ -7,6 +7,11 @@ import SpecializationList from '../containers/SpecializationList'
 export default () => (
   <div>
     <Header />
+    <div id="foreword">
+      <h2>Specialization planner</h2>
+      See how close you are to completing specializations by clicking courses that you've taken
+      and referencing against requirements on the right.
+    </div>
     <div id="app">
       <div id="courses">
         <h2>Courses</h2>

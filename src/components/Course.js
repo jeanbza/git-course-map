fa932e7c7eb96ef367a46d2edbb1329f924a88e5
Waciuma Wanjohi <code@waciuma.com>
2017-10-28 10:16:50 -0400

    Reformat return on moreInformation buttonClicked

--- src/components/Course.js
+++ src/components/Course.js
@@ -48,11 +48,11 @@ class Course extends React.Component {
   extras(buttonClicked, moreInformation) {
     if (buttonClicked && moreInformation) {
       return (
-        <div className="reviewInset">
-          <a href={moreInformation.reviewLink} target="_blank">Course reviews</a>
-        </div>
-        <div className="wikiInset">
-          <a href={moreInformation.wikiLink} target="_blank">Course page on Succeed in OMSCS</a>
+        <div className="inset">
+          <div>
+            <a href={moreInformation.reviewLink} target="_blank">Course reviews</a>
+            <a href={moreInformation.wikiLink} target="_blank">Course Wiki</a>
+          </div>
         </div>
       )
     }
